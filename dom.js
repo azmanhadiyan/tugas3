a05cbcc88d36fad2a9f092fff9c45a12f68daf83
azmanhadiyan <azmanhadiyan@gmail.com>
2021-06-04 17:13:52 +0700

    tugas 3

---
+++ dom.js
@@ -0,0 +1,15 @@
+function myFunction() {
+   document.querySelector("#profil").innerHTML = " My Profil";
+   document.querySelector("#profil").style.fontSize = "30px";
+  }
+
+  function bigImg(x) {
+    x.style.height = "400px";
+    x.style.width = "400px";
+  }
+
+  function normalImg(x) {
+    x.style.height = "250px";
+    x.style.width = "250px";
+  }
+  
